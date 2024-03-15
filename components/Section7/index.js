c362f7688c204248cce0100a8960eecd63a711f9
emraheskbgc <emraheskibagci595@gmail.com>
2024-03-15 16:08:13 +0300

    yorum kısmı halledildi

--- components/Section7/index.js
+++ components/Section7/index.js
@@ -1,9 +1,25 @@
-import React from "react";
+"use client"
+import React, { useRef } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { IoIosStar } from "react-icons/io";
-import styles from "./styles.module.css"
-
+import styles from "./styles.module.css";
+import datas from "@/public/assets/data/section7.json";
 
 function Section7() {
+  const scrollRef = useRef(null);
+
+  const handleScrollLeft = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft -= 200; // Adjust the scroll amount as needed
+    }
+  };
+
+  const handleScrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft += 200; // Adjust the scroll amount as needed
+    }
+  };
+
   return (
     <div>
       <div className="flex justify-center text-[60px] font-[500]">
@@ -12,46 +28,51 @@ function Section7() {
           <span className="text-redTitle ml-10 mt-[-20px]">reviews</span>
         </h1>
       </div>
-      <div className="flex">
-      <div className="bg-grayIcon p-5 rounded-2xl w-96 h-56">
-        <div className="flex px-5 py-5  items-center ">
-          <div className="mr-5">
-            <img src="/assets/images/head.png" className="w-12 h-10 rounded-full" alt="" />
-          </div>
-          <div className=" w-full">
-            <div className="text-darkRed font-[500] uppercase text-md">emrah eskibağcı  </div>
-            <div className="flex justify-between space-x-4  ">
-              <div className="flex space-x-1 text-2xl text-starYellow">
-                <IoIosStar/>
-                <IoIosStar/>
-                <IoIosStar/>
-                <IoIosStar/>
-                <IoIosStar/>
+     
+        <div ref={scrollRef} className={`flex space-x-10 mt-10 overflow-x-auto ${styles.scrollYOff}`}>
+          {datas.map((data) => (
+            <div key={data.id} className="bg-grayIcon p-5 rounded-2xl ml-4">
+              <div className="flex w-80 items-center">
+                <div className="mr-5">
+                  <img src={data.profileImage} className="w-12 h-10 rounded-full" alt="" />
+                </div>
+                <div className="w-full">
+                  <div className="text-darkRed font-[500] uppercase text-md">{data.name} </div>
+                  <div className="flex justify-between space-x-4 ">
+                    <div className="flex space-x-1 text-2xl text-starYellow">
+                      <IoIosStar />
+                      <IoIosStar />
+                      <IoIosStar />
+                      <IoIosStar />
+                      <IoIosStar />
+                    </div>
+                    <div className="text-commentTimeText">4 hour ago</div>
+                  </div>
+                </div>
               </div>
-              <div className="text-commentTimeText" >4 hour ago</div>
+              <div className={`overflow-x-auto max-h-24 mt-4 text-commentTimeText ${styles.scrollStyle}`}>
+                <p>
+                  {data.comment}
+                </p>
+              </div>
             </div>
-          </div>
-         
+          ))}
         </div>
-        <div className={`overflow-x-auto max-h-24 text-commentTimeText ${styles.scrollStyle}`}>
-          <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-        </div>
-      </div>
-      <div className="bg-grayIcon ml-4">
-      <div>
-        <div>avatar</div>
-        <div>
-          <div>name</div>
-          <div>stars</div>
-        </div>
-        <div>time</div>
-      </div>
-      <div>comment</div>
-    </div>
-      </div>
       
+      <div className="flex justify-center mt-10">
+        <button
+          onClick={handleScrollLeft}
+          className="bg-grayIcon text-white w-10 h-10 flex justify-center items-center rounded-full opacity-75 hover:opacity-100"
+        >
+          <IoIosArrowBack />
+        </button>
+        <button
+          onClick={handleScrollRight}
+          className="bg-grayIcon text-white w-10 h-10 flex justify-center items-center rounded-full opacity-75 hover:opacity-100"
+        >
+          <IoIosArrowForward />
+        </button>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,12 @@ module.exports = {
         'daire1-bg': "url('/assets/images/daire1.png')",
         'daire2-bg': "url('/assets/images/daire2.png')",
         'daire3-bg': "url('/assets/images/daire3.jpeg')",
-        'otherFea1':"url('/assets/images/otherfeatured1.jpeg')",
-        'otherFea2':"url('/assets/images/otherfeatured2.png)",
-        'otherFea3':"url('/assets/images/otherfeatured3.png)",
-        'otherFea4':"url('/assets/images/otherfeatured4.png)",
-        'footer-bg':"url('/assets/images/footerImage.png')"
+        'otherFea1': "url('/assets/images/otherfeatured1.jpeg')",
+        'otherFea2': "url('/assets/images/otherfeatured2.png')",
+        'otherFea3': "url('/assets/images/otherfeatured3.png')",
+        'otherFea4': "url('/assets/images/otherfeatured4.png')",
+        
+        'footer-bg': "url('/assets/images/footerImage.png')"
       },
       
     },
