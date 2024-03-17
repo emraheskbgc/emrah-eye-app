32a5e7ad95f1d464fceeb68c09c1406eaf280891
emraheskbgc <emraheskibagci595@gmail.com>
2024-03-17 20:59:09 +0300

    yorum kısmına nokta eklendi

--- components/Section7/index.js
+++ components/Section7/index.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { IoIosStar } from "react-icons/io";
 import styles from "./styles.module.css";
@@ -7,16 +7,19 @@ import datas from "@/public/assets/data/section7.json";
 
 function Section7() {
   const scrollRef = useRef(null);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  const handleScrollLeft = () => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollLeft -= 200; // Adjust the scroll amount as needed
-    }
-  };
 
-  const handleScrollRight = () => {
+
+  const scrollToIndex = (index) => {
+    setActiveIndex(index);
     if (scrollRef.current) {
-      scrollRef.current.scrollLeft += 200; // Adjust the scroll amount as needed
+      const scrollWidth = scrollRef.current.offsetWidth;
+      const newPosition = index * (scrollWidth / datas.length); // Her gösterge noktası için bir bölüm
+      scrollRef.current.scrollTo({
+        left: newPosition,
+        behavior: "smooth",
+      });
     }
   };
 
@@ -29,49 +32,44 @@ function Section7() {
         </h1>
       </div>
      
-        <div ref={scrollRef} className={`flex space-x-10 mt-10 overflow-x-auto ${styles.scrollYOff}`}>
-          {datas.map((data) => (
-            <div key={data.id} className="bg-grayIcon p-5 rounded-2xl ml-4">
-              <div className="flex w-80 items-center">
-                <div className="mr-5">
-                  <img src={data.profileImage} className="w-12 h-10 rounded-full" alt="" />
-                </div>
-                <div className="w-full">
-                  <div className="text-darkRed font-[500] uppercase text-md">{data.name} </div>
-                  <div className="flex justify-between space-x-4 ">
-                    <div className="flex space-x-1 text-2xl text-starYellow">
-                      <IoIosStar />
-                      <IoIosStar />
-                      <IoIosStar />
-                      <IoIosStar />
-                      <IoIosStar />
-                    </div>
-                    <div className="text-commentTimeText">4 hour ago</div>
-                  </div>
-                </div>
+      <div ref={scrollRef} className={`flex space-x-10 mt-10 ml-20 overflow-x-auto ${styles.scrollYOff}`}>
+        {datas.map((data, index) => (
+          <div key={data.id} className="bg-grayIcon p-5 rounded-2xl ml-4">
+            <div className="flex w-80 items-center">
+              <div className="mr-5">
+                <img src={data.profileImage} className="w-12 h-10 rounded-full" alt="" />
               </div>
-              <div className={`overflow-x-auto max-h-24 mt-4 text-commentTimeText ${styles.scrollStyle}`}>
-                <p>
-                  {data.comment}
-                </p>
+              <div className="w-full">
+                <div className="text-darkRed font-[500] uppercase text-md">{data.name} </div>
+                <div className="flex justify-between space-x-4 ">
+                  <div className="flex space-x-1 text-2xl text-starYellow">
+                    <IoIosStar />
+                    <IoIosStar />
+                    <IoIosStar />
+                    <IoIosStar />
+                    <IoIosStar />
+                  </div>
+                  <div className="text-commentTimeText">4 hour ago</div>
+                </div>
               </div>
             </div>
-          ))}
-        </div>
-      
-      <div className="md:flex justify-start ml-5 space-x-2 mt-10 hidden ">
-        <button
-          onClick={handleScrollLeft}
-          className="bg-grayIcon text-white w-10 h-10 flex justify-center items-center rounded-full opacity-75 hover:opacity-100"
-        >
-          <IoIosArrowBack />
-        </button>
-        <button
-          onClick={handleScrollRight}
-          className="bg-grayIcon text-white w-10 h-10 flex justify-center items-center rounded-full opacity-75 hover:opacity-100"
-        >
-          <IoIosArrowForward />
-        </button>
+            <div className={`overflow-x-auto max-h-24 mt-4 text-commentTimeText ${styles.scrollStyle}`}>
+              <p>
+                {data.comment}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex justify-start space-x-1 mt-6 ml-24">
+        {datas.map((_, index) => (
+          <button
+            key={index}
+            className={`h-3  rounded-full  ${activeIndex === index ? 'bg-darkRed  w-6 ' : 'bg-darkRed w-3'}`}
+            onClick={() => scrollToIndex(index)}
+          />
+        ))}
       </div>
     </div>
   );
