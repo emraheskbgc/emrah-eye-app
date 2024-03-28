cb29d7d3bb0c053d02f788d5e7dfffbe37ee8384
emraheskbgc <emraheskibagci595@gmail.com>
2024-03-29 02:53:37 +0300

    read more iconuna transition verildi

--- components/Section3/index.js
+++ components/Section3/index.js
@@ -3,6 +3,7 @@ import data from "@/public/assets/data/section3.json";
 import { HiArrowLongRight } from "react-icons/hi2";
 
 function Section3() {
+  1
   return (
     <div className=" flex flex-col justify-center p-5 md:mt-[100px] mt-[120px]  mb-16 ">
       <span className="font-libre-bodoni text-[60px] hidden  font-[500] flex-col md:flex justify-center items-center">
@@ -27,11 +28,11 @@ function Section3() {
                 {data.title}
               </h1>
               <p className="text-center font-lexend ">{data.description}</p>
-              <div className="flex space-x-4  justify-end items-end text-redTitle">
-                <div className="flex relative items-center justify-center">
+              <div className="flex space-x-4  justify-end items-end text-redTitle opacity-65 duration-300 hover:opacity-100 cursor-pointer">
+                <div className="flex relative items-center justify-center ">
                   <div className="font-[500] '"><span className="font-libre-bodoni">Read more</span></div>
-                  <div className="text-[40px]   ">
-                    <HiArrowLongRight className="text-2xl w-16 h-10 stroke-0" />
+                  <div className="text-[40px]    ">
+                    <HiArrowLongRight className="text-2xl w-16 h-10 stroke-0 transition-all duration-300 transform hover:translate-x-2 " />
                   </div>
                   <div className="w-8 h-8 absolute right-0 mr-1 rounded-full border border-redTitle"></div>
                 </div>
